30f643e28a01ea8aa639577f4bf8e20dde33d6fc
juliandavidmr <anlijudavid@hotmail.com>
2017-03-11 18:27:49 -0500

    upload files to npm

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var uhunt = require("./lib/uhunt.lib");
-
-uhunt.byId("161").then((data) => {
-    console.log(data);
-}, error => {
-    console.log(error);
-})
+module.exports = require("./lib/uhunt.lib");
