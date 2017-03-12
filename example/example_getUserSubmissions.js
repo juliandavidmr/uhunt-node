f59ab261546b3b763c6c8521d84714a12c6b0d2d
juliandavidmr <anlijudavid@hotmail.com>
2017-03-12 12:56:48 -0500

    improvements submissions

--- example/example_getUserSubmissions.js
+++ example/example_getUserSubmissions.js
@@ -1,7 +1,7 @@
 var uhunt = require("../lib/uhunt.lib");
 
 // Get data submissions from any user 
-uhunt.getUserSubmission("339").then((data) => {
+uhunt.getUserSubmission("706667").then((data) => {
     console.log(data);
 }, error => {
     console.log(error);
